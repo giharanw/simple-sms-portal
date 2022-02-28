d7eba4553fd79b1d9af7d7a50e019bc283f70c4f
Gihara Wicktramatunge <giharanw@gmail.com>
2022-02-28 14:38:55 +0530

    Send SMS - 2

--- src/controller/PortalFormController.java
+++ src/controller/PortalFormController.java
@@ -8,8 +8,13 @@ import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Optional;
 
 public class PortalFormController {
@@ -70,40 +75,71 @@ public class PortalFormController {
     }
     
     public void btnSendOnAction(ActionEvent event) throws IOException {
-        Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
-        confirmation.setTitle("Confirmation");
-        confirmation.setHeaderText("Your message and contact(s) are as follows: ");
-        confirmation.setContentText(new String("Message: "+txtMessage.getText()+"\nContact(s): "+lstContacts.getItems().toString()));
-        confirmation.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
-        Optional<ButtonType> result = confirmation.showAndWait();
-        if (result.get()==ButtonType.OK){
-            URL url = new URL("https://api.smshub.lk/api/v2/send/single");
+        if (sendAlert(Alert.AlertType.CONFIRMATION,"Confirmation","Your message and contact(s) are as follows: ","Message: "+txtMessage.getText()+"\nContact(s): "+lstContacts.getItems().toString()).get()==ButtonType.OK){
+            int size= lstContacts.getItems().size();
+            URL url = new URL(setURL(size));
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-type","application/json");
-            connection.setRequestProperty("","application/json");
-
-            String apiToken = "Enter API Token Here";
-
+            connection.setRequestProperty("Content-Type","application/json");
+            String apiToken = "5499dulq9EZvFVNHsO4oufAqBtQTr4tj7WZYgkqskF9L9EQrcUhSm10OtBw3UOnVGoScqBmXIIKM5eCGsLdyu4CUmWfxwXHM10pMUtGKZ5OPhFSauEXWUqpfq07YW9j2Fu3Hf36uz8ShSVBNHtS9TKInYLRGrveo6a7MO5ftvU4t6Ha5T65wahDEdh1OCkghJclAH1Nj6ExcMm5BOWzseFlMALqIcmPc0xRCd02qhNVbv6WQRBEhlPnnogWG7yWwtxtFF087xkaGlhcyT6dCMXMmJyM6sa7cSNCyxaZA52K3Oi4PA798pswC9tlvBMbLPPqKxZ6N7pz8U2S9thdMKJyQJsG6FFXBaIlE65fvdnMmxsdQHSx8Enee6utzGigczFUclT6be6RQDqksUPmNdOxlXrU6F3WOjUdZ7v3BodGHyvfW2yh6XsDSpVF7OaYnIbB0kgxULV55bYpy6UdrK2j2QTwlEvPcKg59eZen0wdLtwJjEPxCXW2hEQeSXgelpB2rgvAGbUnB9ViqlJjCVc0ovA4sArNs0stcR4Qiw0o2nyhcL51SQb8Adm8axh2m2Tk49tdJzZu8hLB2jZ1mmTMcQwQqr1aeU7mv8Uh2S79wOFBKA2yhTCf1kG73KgaK8leltQVRBNeJwR023gT5k72udiS3COQHNBE03WDfhdobVpT9oU7d5wLmGMh31AsAsNrq9beLWo3qU12hzfYamjbyWyxPlru68FFciVpAG1jo4oYCfz8lUCy1UPv9CL0MuTsm0uNfbxnIy5MttCfFHJQRXHDivPFHjRSaS0mVv8AmXzRtjBIlM4oM8uJohBZm4lZLeUTnXHChANxolPo3NOMkjbsz826uuraeyzuCaISGdt8C9GpoV8vRmjvBdDI4OW7ZcwBr1TJsO3sAZY8FpbyKprhAqRfzIxtNqpJuqgpVBnVCQNDIUkPWwzhjGhi9ozhaD4YSOjl3lU3zluzvS7chT8fQJf0aCfEqU4rF2Cesmto3ROTibGFfu1XRjkAy";
             connection.setRequestProperty("Authorization", apiToken);
 
-            if (lstContacts.getItems().size()==1){
-                sendSMSSingleContact();
-            }
-            else if (lstContacts.getItems().size()>1){
-                sendSMSMultipleContacts();
-            }
+            if (size==0) sendSMSSingleContact(size,connection);
+            else if (size>1) sendSMSMultipleContacts(size,connection);
         }
     }
 
-    private void sendSMSSingleContact() {
+    private Optional<ButtonType> sendAlert(Alert.AlertType alertType, String title, String headerText, String contentText) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        Optional<ButtonType> result = alert.showAndWait();
+        return result;
+    }
+
+    private String setURL(int size) {
+        String urlSpec=size==1?"https://api.smshub.lk/api/v2/send/single":size>1?"https://api.smshub.lk/api/v2/send/bulk":"";
+        return urlSpec;
+    }
+
+    private void sendSMSSingleContact(int size, HttpURLConnection connection) {
         /* Todo: Implement sendSMSSingleContact */
         System.out.println("sendSMSSingleContact()");
     }
 
-    private void sendSMSMultipleContacts() {
+    private void sendSMSMultipleContacts(int size, HttpURLConnection connection) throws IOException {
         /* Todo: Implement sendSMSMultipleContacts */
-        System.out.println("sendSMSMultipleContact()");
+        try {
+            StringBuilder data = new StringBuilder("{\n\t\"messageArray\": [");
+            for (String contactsItem : lstContacts.getItems()) {
+                data.append("\n\t\t{\n\t\t\t\"PhoneNumber\": \""+contactsItem+"\"," +
+                        "\n\t\t\t\"SmsMessssage\": \""+txtMessage.getText()+"\"\n\t\t},");
+            }
+            data.deleteCharAt(data.length()-1);
+            data.append("\n\t]\n}");
+
+            connection.setDoOutput(true);
+            OutputStream os = connection.getOutputStream();
+            os.write(String.valueOf(data).getBytes());
+            os.close();
+
+            InputStream is = connection.getInputStream();
+            StringBuilder response = new StringBuilder();
+
+            ReadableByteChannel byteChannel = Channels.newChannel(is);
+            while (true) {
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                int read = byteChannel.read(buffer);
+                if (read!=-1) response.append(buffer);
+                else break;
+            }
+            byteChannel.close();
+            sendAlert(Alert.AlertType.INFORMATION,"Information!",String.valueOf(connection.getResponseCode()),"Message sent successfully...");
+        } catch (Throwable e) {
+            sendAlert(Alert.AlertType.ERROR,"Error!","",connection.getResponseMessage());
+        }
     }
 
 }
